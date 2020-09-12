af33722ade21fd0f98dc51c36607f04d4334926b
varunpreet-code <cp0452472@gmail.com>
2020-09-12 12:37:24 +0800

    Complete function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from "react";
-import TodoItem from "./components/TodoItem";
+import React, {useState} from "react";
+import TodoItem from "./components/TodoItem/TodoItem";
+import TodoForm from "./components/TodoForm/TodoForm";
+import "./App.css";
 
 function App() {
-  return (<div>
+
+  // variables
+const [todos, setTodos] = useState([
+  { text: "Finish plus project", iscompleted: false },
+  { text: "Feed peggy", iscompleted: false },
+  { text: "Sleep", iscompleted: false },
+]);
+
+// methods
+
+const addTodo = (text) => {
+  const newtodos = [...todos,{ text }];
+  setTodos(newtodos);
+};
+
+const completeTodo = (index) => {
+  const newTodos = [...todos];
+  newTodos[index].isCompleted = true;
+  setTodos(newTodos);
+
+};
+
+const removeTodo = (index) => {
+  const newTodos = [...todos];
+  newTodos.splice(index, 1);
+  setTodos(newTodos);
+};
+
+  // temlate
+  return (
+  <div className="app">
+    <div className="todo-list">
+      <div>
     <h1>My todo list</h1>
-    <TodoItem  todo="Finish plus project."/>
-    <TodoItem  todo="Feed peggy."/>
-    <TodoItem  todo="Sleep."/>
+    {todos.map((todo, index) => (
+      <TodoItem 
+      todo={todo} 
+      key={index}
+      index={index}
+      completeTodo={completeTodo}
+      removeTodo={removeTodo}
+      />
+
+    ))}
+    <TodoForm addTodo={addTodo} />
+      </div>
     </div>
+  </div>
   );
 
 }
---
+++ src/components/TodoForm/TodoForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+
+function TodoForm (props) {
+    const { addTodo } = props;
+    const [value, setValue] = useState("");
+    
+    // methods
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!value) {
+            return;
+        }
+        addTodo(value)
+        setValue("")
+    };
+
+// template
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+            value ={value}
+            type="text" 
+            placeholder="Add Todo...." 
+            onChange={(event) => setValue(event.target.value)}
+            />
+        </form>
+    );
+}
+
+
+export default TodoForm;
